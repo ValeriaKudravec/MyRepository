3b2898783dd71202d04de31425cd425ebe6026c8
ValeriaKudravec <valeriakudravec@gmail.com>
2019-03-16 10:22:40 +0300

    Add photoPost File

---
+++ UI/photoPost.js
@@ -0,0 +1,392 @@
+var photoPosts = [
+
+    {
+  
+      id: '1',
+  
+      descriprion: 'Первый пост',
+  
+      createdAt:  Date.parse('2018-02-23T23:00:00'),
+  
+      author: 'Иванов Иван',
+  
+      photoLink: '41_NQcBBMCI.jpg',
+      likes: ['petrov', 'ivanov'],
+      hashtag:['pain', 'rain', 'again'],
+
+     },
+     {
+  
+      id: '2',
+  
+      descriprion: 'Второй пост',
+  
+      createdAt:  Date.parse('2018-01-23T23:01:00'),
+  
+      author: 'Петров Иван',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['second', 'post', 'again'],
+
+     },
+     {
+  
+      id: '3',
+  
+      descriprion: 'Третий пост',
+  
+      createdAt:  Date.parse('2017-02-23T23:05:00'),
+  
+      author: 'Петров петр',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/931389/91ddf199-d34b-4f7d-b9e3-c19c05d80767/s1200',
+      likes: ['ivanov', 'сидоров'],
+      hashtag:['Third', 'post'],
+     },
+     {
+  
+      id: '4',
+  
+      descriprion: 'Четвертый пост',
+  
+      createdAt:  Date.parse('2018-03-08T23:01:00'),
+  
+      author: 'User4',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/805781/d1df9cd5-7ef8-48e9-a15f-8e9e502f5d4b/s1200',
+      likes: ['petrov', 'ivanov', 'сидоров', 'user3'],
+      hashtag:['post', 'again'],
+     },
+     {
+  
+      id: '5',
+  
+      descriprion: 'Пятый пост',
+  
+      createdAt:  Date.parse('2018-02-23T23:01:00'),
+  
+      author: 'пользователь',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['hello', 'beauti', 'five'],
+
+     },
+     {
+  
+      id: '6',
+  
+      descriprion: 'Шестой пост',
+  
+      createdAt:  Date.parse('2018-02-23T23:01:00'),
+  
+      author: 'Огайчик Екатерина',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров', 'пользователь'],
+      hashtag:['girl', 'lfl', 'love'],
+     },
+     {
+  
+      id: '7',
+  
+      descriprion: 'seven пост',
+  
+      createdAt:  Date.parse('2018-02-01T23:01:00'),
+  
+      author: 'Блогер',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров', 'Огайчик Екатерина', 'пользователь', 'User4', 'User3'],
+      hashtag:['seven'],
+     },
+     {
+  
+      id: '8',
+  
+      descriprion: '8 пост',
+  
+      createdAt:  Date.parse('2018-04-23T23:01:00'),
+  
+      author: 'Иванов Петр',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['8', 'post'],
+     },
+     {
+  
+      id: '9',
+  
+      descriprion: 'Девятый пост',
+  
+      createdAt:  Date.parse('2018-03-23T23:01:00'),
+  
+      author: 'калека',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров', 'Иванов Петр'],
+      hashtag:['калека', 'post'],
+     },
+     {
+  
+      id: '10',
+  
+      descriprion: 'Десятый пост',
+  
+      createdAt:  Date.parse('2018-02-02T23:01:00'),
+  
+      author: 'Чувак чувачок',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров', 'калека', 'Иванов Петр'],
+      hashtag:['cat', 'dog', 'animals'],
+     },
+     {
+  
+      id: '11',
+  
+      descriprion: 'Одинадцатый пост',
+  
+      createdAt:  Date('2018-02-23T23:01:00'),
+  
+      author: 'тюльпан',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['cat', 'help', 'js'],
+     },
+     {
+  
+      id: '12',
+  
+      descriprion: 'двенадцатый пост',
+  
+      createdAt:  Date.parse('2018-02-23T23:01:00'),
+  
+      author: 'чсв печь',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['krasivo', 'simpatichno', 'again'],
+     },
+     {
+  
+      id: '13',
+  
+      descriprion: 'Тринадцатый пост',
+  
+      createdAt:  Date.parse('2018-02-23T23:01:00'),
+  
+      author: 'уууууужин',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['13', 'post', 'again'],
+     },
+     {
+  
+      id: '14',
+  
+      descriprion: 'Четырнадцатый пост',
+  
+      createdAt:  Date.parse('2018-02-23T23:01:00'),
+  
+      author: 'юджин',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['beauti', 'post', 'smile'],
+     },
+     {
+  
+      id: '15',
+  
+      descriprion: 'евгений мележ',
+  
+      createdAt:  Date.parse('2018-02-03T23:01:00'),
+  
+      author: 'мележ',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov'],
+      hashtag:['second', 'post', 'pain'],
+     },
+     {
+  
+      id: '16',
+  
+      descriprion: '16-ый пост',
+  
+      createdAt:  Date.parse('2018-02-23T23:01:00'),
+  
+      author: 'снеговик',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['rain', 'post', 'dog'],
+     },
+     {
+  
+      id: '17',
+  
+      descriprion: 'рюмка водки',
+  
+      createdAt:  Date.parse('2018-02-23T23:01:00'),
+  
+      author: 'Петрова Ивана',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['second', 'vodka', 'pivo'],
+     },
+     {
+  
+      id: '18',
+  
+      descriprion: 'красотуля',
+  
+      createdAt:  Date('2018-02-23T23:01:00'),
+  
+      author: 'Ивангей',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['its', 'okey', 'to', 'be', 'gay'],
+     },
+     {
+  
+      id: '19',
+  
+      descriprion: 'дивчина',
+  
+      createdAt:  Date('2018-02-23T23:01:00'),
+  
+      author: 'Алесенька Котик',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['pretty', 'nail', 'smile'],
+     },
+     {
+  
+      id: '20',
+  
+      descriprion: 'памагити',
+  
+      createdAt:  Date('2018-02-23T23:01:00'),
+  
+      author: 'можно ненада',
+  
+      photoLink: 'https://avatars.mds.yandex.net/get-pdb/25978/c677b333-0293-4299-96bc-39df0e535ca2/orig',
+      likes: ['petrov', 'ivanov', 'сидоров'],
+      hashtag:['help', 'ne', 'nada'],
+     },
+  ];
+  (function() {
+
+      function comparePost(a, b) {
+            return a.createdAt>b.createdAt ? -1 : a.createdAt<b.createdAt ? 1 : 0;
+          }
+          
+      
+      function getPhotoPost(id) {
+            for(i = 0;i < photoPosts.length; ++i){
+                  if(photoPosts[i].id == id){
+                        alert( photoPosts[i].author );
+                        return photoPosts[i];
+                  }
+            }
+            return null;
+      }
+      function getAuthorPost(skip, top, filterConfig = null){
+            returnArray = [];
+            arr = [];
+            var index = 0;
+                  if(filterConfig!=null){
+                  for(i = 0;i < photoPosts.length; ++i){
+                        if(photoPosts[i].author == filterConfig.author){
+                              arr[index]=photoPosts[i];
+                                    ++index;
+                        }
+                              for(k = 0; k < photoPosts[i].hashtag.length; k++){
+                              if(photoPosts[i].hashtag[k] == filterConfig.hashtag){
+                                    arr[index]=photoPosts[i];
+                                    ++index;
+                              }
+                        }
+                  }
+            
+            if(top+skip > arr.length){
+                  top = arr.length;
+            }
+            for(l = skip; l < top; ++l){
+                  returnArray[l] = arr[l];
+                  alert(returnArray[l].descriprion);
+            }
+            returnArray.sort(comparePost);
+            return returnArray;
+      }
+      if(top+skip > photoPosts.length){
+            top = arr.length;
+      }
+      for(l = skip; l < top; ++l){
+            returnArray[l] = photoPosts[l];
+      }
+      returnArray = returnArray.sort(comparePost);
+     
+      return returnArray;
+      }
+
+      function validatePhotoPost(photoPost){
+            if(photoPost.id == null){
+                  return false;
+            }
+            if(photoPost.descriprion == null){
+                  return false;
+            }
+            if(photoPost.author == null){
+                  return false;
+            }
+            if(photoPost.photoLink == null){
+                  return false;
+            }
+            if(photoPost.createdAt == null){
+                  return false;
+            }
+            return true;
+      }
+      function addPhotoPost(photoPost){
+            if(validatePhotoPost(photoPost)){
+                  photoPosts.push(photoPost);
+            }
+      }
+      function removePhotoPost(id){
+            for(i = 0;i < photoPosts.length; ++i){
+                  if(photoPosts[i].id == id){
+                        alert( photoPosts[i].author );
+                        photoPosts.slice(i, 1);
+                  }
+            }
+      }
+      function editPhotoPost(id, photoPost){
+            post = getPhotoPost(id);
+            if(photoPost.descriprion!=null){
+                  post.descriprion = photoPost.descriprion;
+            }
+            if(photoPost.photoLink != null){
+                  post.photoLink = photoPost.photoLink;
+            }
+            if(photoPost.likes!=null){
+                  post.likes = photoPost.likes;
+            }
+            if(photoPost.hashtag != null){
+                  post.hashtag = photoPost.hashtag;
+            }
+      }
+      editPhotoPost('1', { photoLink: 'http://haradok.info/static/news/5/4565/preview.jpg' })
+      alert(getPhotoPost('1').photoLink);
+     getAuthorPost(0, 10);
+    
+    }());
